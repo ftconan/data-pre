437f16c8138b0ec387c326621a96f9d56278ba15
ftconan <1508907793@qq.com>
2019-06-21 15:53:57 +0800

    update pd_pickle

--- pickle/pd_pickle.py
+++ pickle/pd_pickle.py
@@ -11,25 +11,20 @@ import pandas as pd
 def read_pickle(file, **kwargs):
     """
     read pickle
-    :param file:    pickle
-    :param kwargs:
-    :return: DataFrame
+    :return:
     """
     pickle_df = pd.read_pickle(file, **kwargs)
 
     return pickle_df
 
+# :param file:    pickle
+# :param kwargs:
+# :return: DataFrame
 
 def write_pickle(df, **kwargs):
     """
     write pickle
-    :param df:
-    :param kwargs: file_name:     file name
-                   file_path:     file path
-    :return: {
-        'file_name': file_name,
-        'output_path': output_path
-    }
+    :return:
     """
     file_name = kwargs.get('file_name', '')
     file_path = kwargs.get('file_path', '')
@@ -41,3 +36,11 @@ def write_pickle(df, **kwargs):
         'file_name': file_name,
         'output_path': output_path
     }
+
+# :param df:
+# :param kwargs: file_name:     file name
+#                file_path:     file path
+# :return: {
+#     'file_name': file_name,
+#     'output_path': output_path
+# }
